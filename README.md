8bb6145c2fcea06c301d6ac5b9852cad4dab7fd4
Poter <piotr.poteralski@gmail.com>
2015-01-18 01:45:35 +0100

    new markdown

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # NetworkToolsForAndroid
+
+NetworkToolsForAndroid is a script to install awesome network tools to your android     phone.
+
+Package include:
+  - arpspoof
+  - ettercap
+  - hydra
+  - tcpdump
+  - all required libs 
+
+
+
+### Version
+0.0.1
+
+### Tech
+
+This release uses binarys from dSploit. All bins contains all of the primary network tools for android.
+
+### Installation
+
+```sh
+$ sudo chmod +x install.sh
+$ ./install.sh
+```
+
--- install.sh
+++ install.sh
@@ -19,23 +19,23 @@ adb shell su -c "mount -o rw,remount /system"
 echo "Copying Tools To Device..."
 adb push tools /sdcard/tmp/tools
 echo "Copying Internals..."
-adb shell su -c "cp /sdcard/tmp/tools/arpspoof/arpspoof /system/bin/arpspoof
-adb shell su -c "cp /sdcard/tmp/tools/ettercap/ettercap /system/bin/ettercap
-adb shell su -c "cp /sdcard/tmp/tools/ettercap/etterfilter /system/bin/etterfilter
-adb shell su -c "cp /sdcard/tmp/tools/ettercap/etterlog /system/bin/etterlog
+adb shell su -c "cp /sdcard/tmp/tools/arpspoof/arpspoof /system/bin/arpspoof"
+adb shell su -c "cp /sdcard/tmp/tools/ettercap/ettercap /system/bin/ettercap"
+adb shell su -c "cp /sdcard/tmp/tools/ettercap/etterfilter /system/bin/etterfilter"
+adb shell su -c "cp /sdcard/tmp/tools/ettercap/etterlog /system/bin/etterlog"
 adb shell su -c "mkdir /system/usr/share/ettercap/"
-adb shell su -c "cp /sdcard/tmp/tools/ettercap/share/etter.conf /system/usr/share/ettercap/etter.conf
-adb shell su -c "cp /sdcard/tmp/tools/hydra/hydra /system/bin/hydra
-adb shell su -c "cp /sdcard/tmp/tools/hydra/usernames /system/bin/usernames
-adb shell su -c "cp /sdcard/tmp/tools/tcpdump/tcpdump /system/bin/tcpdump
-adb shell su -c "cp /sdcard/tmp/tools/libs/libcrypto.so /system/lib/libcrypto.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libgif.so /system/lib/libgif.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libltdl.so /system/lib/
-adb shell su -c "cp /sdcard/tmp/tools/libs/libnet.so /system/lib/libnet.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libpcap.so /system/lib/libpcap.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libpcre.so /system/lib/libpcre.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libssh.so /system/lib/libssh.so
-adb shell su -c "cp /sdcard/tmp/tools/libs/libssl.so /system/lib/libssl.so
+adb shell su -c "cp /sdcard/tmp/tools/ettercap/share/etter.conf /system/usr/share/ettercap/etter.conf"
+adb shell su -c "cp /sdcard/tmp/tools/hydra/hydra /system/bin/hydra"
+adb shell su -c "cp /sdcard/tmp/tools/hydra/usernames /system/bin/usernames"
+adb shell su -c "cp /sdcard/tmp/tools/tcpdump/tcpdump /system/bin/tcpdump"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libcrypto.so /system/lib/libcrypto.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libgif.so /system/lib/libgif.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libltdl.so /system/lib/"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libnet.so /system/lib/libnet.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libpcap.so /system/lib/libpcap.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libpcre.so /system/lib/libpcre.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libssh.so /system/lib/libssh.so"
+adb shell su -c "cp /sdcard/tmp/tools/libs/libssl.so /system/lib/libssl.so"
 echo "Removing Temporary Files..."
 adb shell su -c "rm -r /sdcard/tmp/tools"
 echo "Done!"
